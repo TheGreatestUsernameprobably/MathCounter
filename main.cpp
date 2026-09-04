f5a66feba7c492f0c59f8386be2c59f2d66bf49c
TheGreatestUsernameprobably <ya.nikituschka@yandex.ru>
2026-09-04 23:12:20 +0300

    21312

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include<gmpxx.h>
+
+
+int main() {
+    std::cout << " idid something" << "\n";
+    mpz_class a("12332121332132132132131233");
+
+    std::cout << a << std::endl;
+
+    return 0;
+}
